cc50a5092b63d1deea823884c6457b54b169753f
noveogroup-dmeshcheryakov <Dmitriy.Meshcheryakov@noveogroup.com>
2017-05-25 16:54:30 +0600

    Add routes and error handler. Not finished

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const http = require('http');
 const path = require('path');
 const debug = require('debug')('app');
 const config = require('./config');
+const HttpError = require('./error').HttpError;
 
 const app = express();
 app.engine('ejs', require('ejs-locals'));
@@ -18,20 +19,31 @@ if (app.get('env') === 'development') {
 }
 app.use(express.bodyParser());
 app.use(express.cookieParser());
+
+// app.use(require('./middleware/sendHttpError'));
+
 app.use(app.router);
+
+require('./routes')(app);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((err, req, res, next) => {
-  if (app.get('env') === 'development') {
-    const errorHandler = express.errorHandler();
-    errorHandler(err, req, res, next);
+  if (typeof err === 'number') {
+    err = new HttpError(err);
+  }
+
+  if (err instanceof HttpError) {
+    res.sendHttpError(err);
   } else {
-    res.send(500);
+    if (app.get('env') === 'development') {
+      express.errorHandler()(err, req, res, next);
+    } else {
+      err = new HttpError(500);
+      res.sendHttpError(err);
+    }
   }
 });
-
-app.get('/', (req, res) => res.render('index'));
 
 http.createServer(app).listen(config.get('port'), () => {
   debug(`Express server listening on port ${config.get('port')}`);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
+const User = require('../models/user').User;
+const HttpError = require('../error').HttpError;
 
-/*
- * GET home page.
- */
+module.exports = function(app) {
+  app.get('/', (req, res) => res.render('index'));
 
-exports.index = (req, res) => {
-  res.render('index', { title: 'Express' });
+  app.get('/users', (req, res, next) => {
+    User.find({}, (err, users) => {
+      if (err) return next(err);
+      res.json(users);
+    });
+  });
+
+  app.get('/user/:id', (req, res, next) => {
+    User.findById(req.params.id, (err, user) => {
+      if (err) next(err);
+      if (!user) {
+        next(new HttpError(404, 'User not found'));
+      }
+      res.json(user);
+    });
+  });
 };
