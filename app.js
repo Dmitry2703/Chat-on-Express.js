3e34f2b0390fb8578574f95e8d92761678eb8a9e
noveogroup-dmeshcheryakov <Dmitriy.Meshcheryakov@noveogroup.com>
2017-05-25 09:07:38 +0600

    Add templates and vendor libs

--- app.js
+++ app.js
@@ -5,33 +5,34 @@ const debug = require('debug')('app');
 const config = require('./config');
 
 const app = express();
-app.set('port', config.get('port'));
+app.engine('ejs', require('ejs-locals'));
 
-http.createServer(app).listen(app.get('port'), () => {
-  debug('Express server listening on port ' + config.get('port'));
-});
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
 
-app.use((req, res, next) => {
-  if (req.url === '/') {
-    res.end('Hello!')
-  } else {
-    next();
-  }
-});
+app.use(express.favicon());
+if (app.get('env') === 'development') {
+  app.use(express.logger('dev'));
+} else {
+  app.use(express.logger('default'));
+}
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(app.router);
 
-app.use((req, res, next) => {
-  if (req.url === '/forbidden') {
-    next(new Error('wops, access denied!'));
-  } else {
-    next();
-  }
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((err, req, res, next) => {
   if (app.get('env') === 'development') {
     const errorHandler = express.errorHandler();
-    errorHandler(err, req, res, next)
+    errorHandler(err, req, res, next);
   } else {
     res.send(500);
   }
+});
+
+app.get('/', (req, res) => res.render('index'));
+
+http.createServer(app).listen(config.get('port'), () => {
+  debug(`Express server listening on port ${config.get('port')}`);
 });
